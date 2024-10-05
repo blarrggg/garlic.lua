623d84bf240fc99d08bb34d671480cb38592f89f
blarg! <150490946+blarrggg@users.noreply.github.com>
2024-10-04 21:24:11 -0400

    Add files via upload

--- garlic.lua
+++ garlic.lua
@@ -24,7 +24,7 @@ SOFTWARE.
 
 ]]
 
-local garlic = { _VERSION = '1' }
+local garlic = { _VERSION = '2' }
 
 function garlic.newActor(spritesheet)
 
@@ -46,7 +46,11 @@ function garlic.newActor(spritesheet)
         newAnim.currentFrame = 1
 
         for k,v in pairs(anim.frames) do
-            newAnim.frames[k] = love.graphics.newQuad(v.x,v.y,v.w,v.h,spritesheet:getWidth(),spritesheet:getHeight())
+            newAnim.frames[k] = {
+                quad = love.graphics.newQuad(v.x,v.y,v.w,v.h,spritesheet:getWidth(),spritesheet:getHeight()),
+                func = v.attachedFunction,
+                alreadyRan = false
+            }
         end
 
         AvailableAnimations[anim.id] = newAnim
@@ -79,11 +83,21 @@ function garlic.newActor(spritesheet)
     end
 
     function actor.update(dt)
-        for k,v in pairs(AvailableAnimations) do
+        for _,v in pairs(AvailableAnimations) do
             if v.playing then
                 v.currentFrame = v.currentFrame + (v.speed*dt)
 
+                local selectedFrame = v.frames[math.floor(v.currentFrame)]
+                if selectedFrame then
+                    if selectedFrame.func and not selectedFrame.alreadyRan then
+                        selectedFrame.func(v)
+                        selectedFrame.alreadyRan = true
+                    end
+                end
+
                 if v.currentFrame >= (#v.frames + 1) then
+                    for _,f in pairs(v.frames) do f.alreadyRan = false end
+
                     if v.loopback then
                         v.currentFrame = 1
                         v.playing = false
@@ -117,11 +131,11 @@ function garlic.newActor(spritesheet)
 
         if not ChosenAnimationToDraw then -- if the displayed animation is STILL nill, try to draw a fallback animation
             local fallback = AvailableAnimations[fallbackAnim]
-            love.graphics.draw(spritesheet,fallback.frames[ math.floor(fallback.currentFrame) ], unpack({...}))
+            love.graphics.draw(spritesheet,fallback.frames[ math.floor(fallback.currentFrame) ].quad, unpack({...}))
             return 0
         end
         -- otherwise, play the animation like normal
-        love.graphics.draw(spritesheet,ChosenAnimationToDraw.frames[math.floor(ChosenAnimationToDraw.currentFrame)], unpack({...}))
+        love.graphics.draw(spritesheet,ChosenAnimationToDraw.frames[math.floor(ChosenAnimationToDraw.currentFrame)].quad, unpack({...}))
         return 0
     end
 
@@ -138,8 +152,16 @@ function garlic.newAnimation(id,priority,speed,loop,loopback)
 
         local newFrame = {}
         newFrame.x,newFrame.y,newFrame.w,newFrame.h = frame.x,frame.y,frame.w,frame.h
+        newFrame.attachedFunction = frame.getFunction()
 
         animation.frames[FrameIndex] = newFrame
+    end
+    function animation.destroyFrame(index)
+        if (index) then
+            table.remove(animation.frames,index)
+        else
+            table.remove(animation.frames,#animation.frames)
+        end
     end
 
     animation.id = id
@@ -151,13 +173,25 @@ function garlic.newAnimation(id,priority,speed,loop,loopback)
     return animation
 end
 
-function garlic.newFrame(x,y,w,h)
+function garlic.newFrame(x,y,w,h,func)
     local frame = {}
+    local func = func or nil
 
     frame.x = x or 0
     frame.y = y or 0
     frame.w = w or 0
     frame.h = h or 0
+
+    function frame.attachFunction(func)
+        assert(type(func) == 'function','Tried to attach something that was expected to be a function.')
+        func = func
+    end
+    function frame.getFunction()
+        return func
+    end
+    function frame.destroyFunction()
+        func = nil
+    end
 
     return frame
 end
